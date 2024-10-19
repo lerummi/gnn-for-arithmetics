e3fb350b15e72b94bad59050a182983959759bd1
Martin Krause <martin.krause.85@gmx.de>
2024-10-20 01:13:51 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# gnn-for-arithmetics
-Train a graph neural network on doing complex arithmetics
+# Graph Neural Networks (GNNs) for arithmetics
+
+## Introduction
+
+This project serves as a playground to find out whether it is possible to calculate complex arithmetic expressions using GNNs.
+
+The currently ubiquitous LLMs often have problems with generalizability for these kind of problems, because they lack proper 
+parametrization of arithmetics and don't seem to be able to derive an "understanding" of the root problem to be solved.
+
+I have the intuition to be able to train this ability, where the currently ubiquitous LLMs often have problems with generalizability, 
+through the more flexible parameterization in graph structures. 
---
+++ arigin/data_generation.py
@@ -0,0 +1,71 @@
+import random
+
+OPERATORS = ["+", "-", "*", "/"]
+OPEN_PARENTHESIS = "("
+CLOSE_PARENTHESIS = ")"
+
+
+def generate(max_integer: int, max_numbers: int) -> str:
+    """
+    Generate an arithmetic expression by applying the formatting rules,
+    which are hardcoded to fill basic arithmetic rules.
+
+    :param max_integer: Greatest integer to be used in the generated
+                         result.
+    :type max_integer: int
+    :param max_numbers: maximum number of integers to be created. Must be
+                       greater or equal 2.
+    :type max_numbers: int
+
+    :return: String representing the expression.
+    """
+
+    expression = []
+    
+    max_numbers = random.randint(2, max(max_numbers, 2))
+
+    n_open_parentesis = 0
+    n_numbers = 0
+
+    def _number(max_integer: int):
+        return random.randint(1, max_integer)
+
+    while n_numbers < max_numbers:
+        if len(expression):
+            previous_entity = expression[-1]
+        else:
+            previous_entity = None
+
+        if isinstance(previous_entity, int):  # Number
+            next_possible_entity = list(OPERATORS)
+            if n_open_parentesis > 0:
+                next_possible_entity.append(CLOSE_PARENTHESIS)
+            next_entity = random.choice(next_possible_entity)
+        elif previous_entity in OPERATORS:  # Operator
+            next_entity = _number(max_integer)
+        elif previous_entity == CLOSE_PARENTHESIS:  # )
+            next_possible_entity = list(OPERATORS)
+            next_entity = random.choice(next_possible_entity)
+        elif previous_entity == OPEN_PARENTHESIS:  # (
+            next_entity = _number(max_integer)
+        else:  # None <- initial
+            next_possible_entity = [
+                _number(max_integer),
+                OPEN_PARENTHESIS
+            ]
+            next_entity = random.choice(next_possible_entity)
+
+        if next_entity == CLOSE_PARENTHESIS:
+            n_open_parentesis -= 1
+        elif next_entity == OPEN_PARENTHESIS:
+            n_open_parentesis += 1
+        elif isinstance(next_entity, int):
+            n_numbers += 1
+
+        expression.append(next_entity)
+
+    while n_open_parentesis > 0:
+        expression += CLOSE_PARENTHESIS
+        n_open_parentesis -= 1
+
+    return " ".join((str(x) for x in expression))
